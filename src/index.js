0de102b9400118f90b27c06407eb196cbb6790cf
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-12 23:41:46 +0200

    js file changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './style.css';
-
-const likeBtn = document.querySelector('likebtn');
-const likeNim = document.querySelector('like-nim');
-
-
-  likeBtn.addEventListener ('click', ()=>{
-    document.querySelector('likeNim').innerHTML = + 1;
-});
-
