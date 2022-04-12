c5fdae17e75402b859b377e8dd4c738f561ed36a
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-13 00:49:00 +0200

    info

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ import './style.css';
 
 const cards = [
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/BE5A/production/_123303784_gettyimages-524191066.jpg',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
   },
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'http://prod-upp-image-read.ft.com/a4e8f394-313b-11ea-a329-0bcf87a328f2',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
   },
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'https://media.wired.com/photos/61bbc2cbb540f6bc340c4429/3:2/w_1280%2Cc_limit/Gear-Mercedes-EQS-21C0123_001.jpg',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
   },
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
   },
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/new-ghost-white-fr-3-4-1-1598911711.jpg',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
   },
   {
-    pictures: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqkcjDWXc2CFchSIC6Wc704RVe1gmRUuETKq0-5a2iKbsi03yHtuc56ag84wdnYd3EAjE&usqp=CAU',
+    pictures: 'https://www.gannett-cdn.com/media/2019/04/25/USATODAY/usatsports/247WallSt.com-247WS-543507-ms.jpg?crop=1365,768,x0,y0&width=660&height=372&format=pjpg&auto=webp',
     title: 'Card title',
     likes: '20',
     description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.',
