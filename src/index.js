c847f47a3eeb6ac19f1113e3f61af76bc60b83ef
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-04-12 17:06:32 +0100

    create like btn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const likeBtn = document.querySelector('likebtn');
+const likeNim = document.querySelector('like-nim');
+
+
+  likeBtn.addEventListener ('click', ()=>{
+    document.querySelector('likeNim').innerHTML = + 1;
+});
+
