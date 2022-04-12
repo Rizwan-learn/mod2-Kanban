351505ee8c628f75123e19b6651ffca7d6cf21c1
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-12 23:46:33 +0200

    adding cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const cards = [
+  {
+    pictures: 'regiss',
+    title: 'cards',
+    likes: '20',
+    description: 'Some quick example text to build on the card title and make up the bulk of thecard\'s content.'
+  }
+]
