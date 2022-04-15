a94177c6560c520048bffa89842fcf116acf3de1
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-15 15:52:23 +0200

    environment

--- src/index.js
+++ src/index.js
@@ -1,139 +1,30 @@
-/* eslint-disable camelcase */
 import './style.css';
+import {
+  HomeApplication,
+  getAllCarsCount,
+} from './Modules/home/home-app.js';
+import { showCommentModal } from './Modules/comments/comments-app.js';
 
-const url_api = 'https://private-anon-af31af2f40-carsapi1.apiary-mock.com/cars';
+const homeApp = new HomeApplication();
 
-// Get api info and dispay on screen cards
-async function getCars() {
-  const response = await fetch(url_api);
-  const cards = await response.json();
-  const {
-    // eslint-disable-next-line no-unused-vars
-    img_url, model, horsepower, id,
-  } = cards;
+const registerHomeEvents = () => {
+  const likesButtons = document.querySelectorAll('.heart');
+  const allCarsCount = document.querySelector('.cars-nav-element');
 
-  const cardDisplayContainer = document.querySelector('.space-cards');
-  let cardDisplay = '';
+  allCarsCount.textContent = getAllCarsCount();
 
-  // dispay cards
-  // eslint-disable-next-line no-restricted-syntax
-  for (const card of cards) {
-    cardDisplay += `
-      <div class="card card-1 m-3 border-0 card-shad">
-      <img
-        src="${card.img_url}"
-        class="card-img-top" alt="scifi">
-      <div class="card-body">
-        <div class="row">
-          <h5 class="card-title col-md-6 font-weight-bold">${card.model}</h5>
-          <div class="col-md-6 likes-btn likes-styles text-secondary">
-          <button class="like-btn">
-          <span id="icon"><i class="fa-regular fa-thumbs-up"></i></i></span>
-          <span id="count"> 0 </span> Like
-          </button> 
-          </div>
-        </div>
-        <p class="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of thecard's content.</p>
-        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal" id="${id}">Comments</button>
-        <a href="#" class="btn btn-success rounded-0">Reservations</a>
-      </div>
-    </div>
-      `;
-  }
-  cardDisplayContainer.innerHTML = cardDisplay;
+  likesButtons.forEach((likeButton) => likeButton.addEventListener(
+    'click',
+    homeApp.toggleHeart,
+  ));
 
-  // comment popup display
-  const modal = () => {
-    const modaldisplay = document.querySelector('.modal');
-    const item = cards.find((p) => p.id.toString() === id);
-    // const {
-    //   img_url, model, horsepower, id,
-    // } = item;
+  const commentButtons = document.querySelectorAll(
+    '.car-buttons > .comment-button',
+  );
 
-    modaldisplay.innerHTML = `
-    <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalPopoversLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="image-popup">
-          <img src="https://i.insider.com/592f4169b74af41b008b5977?width=700" alt="car">
-        </div>
-        <div class="d-flex justify-content-center mt-2 text-secondary">
-          <ul>
-            <li>Fuel: Titanum</li>
-            <li>Weight: 400</li>
-          </ul>
-          <ul>
-            <li>length: 100m,000</li>
-            <li>Power: 100,000,000</li>
-          </ul>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-8">
-            <form action="dfd">
-              <h5>Add a comment</h5>
-              <div class="">
-                <label for="name" class="form-label">Your name:</label>
-                <input type="text" class="form-control">
-              </div>
-              <div>
-                <label for="comment">Your insights:</label>
-                <textarea class="form-control" rows="5" id="comment" name="text"></textarea>
-              </div>
-            </form>
-          </div>
-          <div class="col-md-4 comments-bg">
-            <h5>Comment <span class="badge-1 bg-danger">170</span></h5>
-            <ul class="text-secondary commentsall p-0">
-              <li>03/11/2019 <span>Regiss:</span>
-                <p>I'd love to buy it</p>
-              </li>
-              <li>23/03/2022 <span>Toussaint:</span>
-                <p>I'd love to buy it</p>
-              </li>
-              <li>23/03/2022 <span>Toussaint:</span>
-                <p>I'd love to buy it</p>
-              </li>
-              <li>23/03/2022 <span>Toussaint:</span>
-                <p>I'd love to buy it</p>
-              </li>
-              <li><a href="#" type="button" class="text-primary">See More</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer me-auto">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Comment</button>
-      </div>
-    </div>
-  </div>
-    `;
-  };
-  modal();
-}
-getCars();
+  commentButtons.forEach((btn) => btn.addEventListener('click', showCommentModal));
+};
 
-// like buttons
-
-const likeBtn = document.querySelector('.like-btn');
-const likeIcon = document.querySelector('#icon');
-const count = document.querySelector('#count');
-
-let clicked = false;
-
-likeBtn.addEventListener('click', () => {
-  if (!clicked) {
-    clicked = true;
-    likeIcon.innerHTML = '<i class="fa-solid fa-thumbs-up"></i>';
-    count.textContent++;
-  } else {
-    clicked = false;
-    likeIcon.innerHTML = '<i class="fa-regular fa-thumbs-up"></i>';
-    count.textContent--;
-  }
+homeApp.initialize().then(() => {
+  registerHomeEvents();
 });
